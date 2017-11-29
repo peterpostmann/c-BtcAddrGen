c5b91862910a5380ae772052b4d1ea5705a06972
Peter Postmann <peter@postmann.org>
2017-11-29 22:53:13 +0100

    Merge Wrapper

--- BtcAddrGen.c
+++ BtcAddrGen.c
@@ -1,58 +1,26 @@
 
 #include "BtcAddrGen.h"
 
-
-// --- ECDSA
-
 #include "lib/micro-ecc/uECC.h"
-
-#if SECP256K1_KEY_SIZE != uECC_BYTES
-#error "Configuration Error: SECP256K1_KEY_SIZE must match uECC_BYTES"
-#endif
-
-int Secp256k1(const Secp256k1Key *privateKey, Secp256k1UncompressedPublicKey *publicKey)
-{	
-	return uECC_compute_public_key(privateKey, publicKey);
-}
-
-
-// --- SHA256
-
 #include "lib/sha256/sha256.h"
-
-void Sha256(const uint8_t *data, uint32_t len, Sha256Hash *hash)
-{
-    sha256_context ctx;
-
-    sha256_init(&ctx);
-    sha256_hash(&ctx, data, len);
-    sha256_done(&ctx, hash);
-}
-
-
-// --- RIPEMD160
-
 #include "lib/ripemd160/rmd160.h"
-
-void Ripemd160(const uint8_t *data, uint32_t len, Ripemd160Hash *hash)
-{
-    RIPEMD160Context ctx;
-
-    RMD160Init(&ctx);
-    RMD160Update(&ctx, data, len);
-    RMD160Final(hash, &ctx);
-}
-
-// --- BTC
 
 void BtcRaw(const BtcPrivateKey *privateKey, BtcAddressRaw *addressRaw)
 {
+    uint32_t buf[26];
+
     BtcUncompressedPublicKey pubKey = { .version = 0x04 };
-    Secp256k1(privateKey, &(pubKey.key));
+    uECC_compute_public_key(privateKey, &(pubKey.key));
 
     Sha256Hash hash;
-    Sha256(&pubKey, BTC_UNCOMPRESSED_PUBLIC_KEY_SIZE, &hash);
 
-    Ripemd160(&hash, SHA256_HASH_SIZE, addressRaw);
+    sha256_init(&buf);
+    sha256_hash(&buf, &pubKey, BTC_UNCOMPRESSED_PUBLIC_KEY_SIZE);
+    sha256_done(&buf, &hash);
+
+    RMD160Init(&buf);
+    RMD160Update(&buf, &hash, SHA256_HASH_SIZE);
+    RMD160Final(addressRaw, &buf);
+
     addressRaw->network = 0x00;
 }
